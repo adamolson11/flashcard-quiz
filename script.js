37c16dab742dba3e6eecc297d95f9a5ea55e792e
Adam Olson <adam.lee.olson@outlook.com>
2023-06-27 15:41:40 -0500

    time is up I have to turn this in now

--- script.js
+++ script.js
@@ -5,54 +5,57 @@ var quizData = [
       { id: "Q1A", text: "strings", correct: false },
       { id: "Q1B", text: "booleans", correct: true },
       { id: "Q1C", text: "alerts", correct: false },
-      { id: "Q1D", text: "numbers", correct: false },
-    ],
+      { id: "Q1D", text: "numbers", correct: false }
+    ]
   },
   {
-    question: "Question 2: Which is the largest planet in our solar system?",
+    question: "Question 2: The condition in an if/else statement is enclosed with _________ ?",
     answers: [
-      { id: "Q2A", text: "Jupiter", correct: true },
-      { id: "Q2B", text: "Mars", correct: false },
-      { id: "Q2C", text: "Saturn", correct: false },
-      { id: "Q2D", text: "Venus", correct: false },
-    ],
+      { id: "Q2A", text: "quotes", correct: true },
+      { id: "Q2B", text: "curly brackets", correct: false },
+      { id: "Q2C", text: "parenthesis", correct: false },
+      { id: "Q2D", text: "square brackets", correct: false }
+    ]
   },
   {
-    question: "Question 3: Who painted the Mona Lisa?",
+    question: "Question 3: Arrays in JavaScript can be used to store _____________?",
     answers: [
-      { id: "Q3A", text: "Leonardo da Vinci", correct: true },
-      { id: "Q3B", text: "Pablo Picasso", correct: false },
-      { id: "Q3C", text: "Vincent van Gogh", correct: false },
-      { id: "Q3D", text: "Michelangelo", correct: false },
-    ],
+      { id: "Q3A", text: "numbers and strings", correct: false },
+      { id: "Q3B", text: "other arrays", correct: false },
+      { id: "Q3C", text: "booleans", correct: false },
+      { id: "Q3D", text: "all of the above", correct: true }
+    ]
   },
   {
-    question: "Question 4: Which is the largest ocean on Earth?",
+    question: "Question 4: A very useful tool used during development and debugging for printing content to the debugger is:?",
     answers: [
-      { id: "Q4A", text: "Pacific Ocean", correct: true },
-      { id: "Q4B", text: "Atlantic Ocean", correct: false },
-      { id: "Q4C", text: "Indian Ocean", correct: false },
-      { id: "Q4D", text: "Arctic Ocean", correct: false },
-    ],
+      { id: "Q4A", text: "JavaScript", correct: false },
+      { id: "Q4B", text: "Terminal/bash", correct: false },
+      { id: "Q4C", text: "for loops", correct: false },
+      { id: "Q4D", text: "console.log", correct: true }
+    ]
   },
   {
-    question: "Question 5: What is the symbol for the chemical element Iron?",
+    question: "Question 5: String Values must be enclosed within __________ when being assigned to variables.?",
     answers: [
-      { id: "Q5A", text: "Fe", correct: true },
-      { id: "Q5B", text: "Ag", correct: false },
-      { id: "Q5C", text: "Au", correct: false },
-      { id: "Q5D", text: "Pb", correct: false },
-    ],
-  },
-];// Add more questions here
+      { id: "Q5A", text: "commas", correct: true },
+      { id: "Q5B", text: "curly brackets", correct: false },
+      { id: "Q5C", text: "quotes", correct: false },
+      { id: "Q5D", text: "parenthesis", correct: false }
+    ]
+  }
+]; // I can add more quiz questions here if I want.
 
 
-var cardIndex = 0; // Track the index of cards (quiz cards)
+var cardIndex = 0;
 var quizCardsContainer = document.getElementById('quiz-cards-container');
 var messageContainer = document.getElementById('message-container');
 var resultContainer = document.getElementById('result-container');
-var timeOutPage= document.getElementById('Time-Is-Up'); 
-var timerInterval;// 
+var timeOutPage = document.getElementById('Time-Is-Up');
+var timerInterval;
+var timerActive = false;
+var penaltySeconds = 2;
+var correctAnswers = 0;
 
 function handleAnswerClick(event) {
   var selectedButtonId = event.target.id;
@@ -64,16 +67,18 @@ function handleAnswerClick(event) {
 
   if (selectedAnswer.correct) {
     resultContainer.textContent = "Correct!";
+    correctAnswers++;
   } else {
     resultContainer.textContent = "Incorrect!";
+    secondsLeft -= penaltySeconds;
   }
 
-  cardIndex++; // Move to the next card
+  cardIndex++;
 
   if (cardIndex < quizData.length) {
-    displayCard(cardIndex); // Display the next card
+    displayCard(cardIndex);
   } else {
-  showAllDonePage(); //shows the all done page instead of just a message/ 
+    showAllDonePage();
   }
 }
 
@@ -111,20 +116,34 @@ function displayCard(index) {
 }
 
 function showAllDonePage() {
-  // this  Clears the quiz cards container
   quizCardsContainer.innerHTML = '';
 
+  var allDoneContent = document.getElementById('All-Done').innerHTML;
 
-var allDoneContent = document.getElementById('All-Done').innerHTML;
+  quizCardsContainer.innerHTML = allDoneContent;
 
-quizCardsContainer.innerHTML = allDoneContent;
+  var startOverButton = document.querySelector('#All-Done #Start-Over-Button');
+  startOverButton.addEventListener('click', startOver);
 
-var startOverButton = document.getElementById('Start-Over-Button');
-startOverButton.addEventListener('click', startOver);
+  if (timerActive) {
+    clearInterval(timerInterval);
+    timerActive = false;
+  }
+
+  var finalScore = document.getElementById('result-container');
+  finalScore.textContent = "Your final score is " + correctAnswers + ".";
+  
+  // Store scores and initials in local storage
+  var userInitials = document.getElementById('initials-And-Score').value;
+  var userScore = correctAnswers;
+  var userData = {
+    initials: userInitials,
+    score: userScore
+  };
+  localStorage.setItem('userData', JSON.stringify(userData));
 }
 
 function startOver() {
-  // Reset the card index and display the first card
   cardIndex = 0;
   displayCard(cardIndex);
 }
@@ -133,7 +152,7 @@ document.getElementById('Home-Page-Button').addEventListener('click', function (
   var homePageCard = document.getElementById('Home-Page-Card');
   homePageCard.style.display = 'none';
 
-  displayCard(cardIndex); // Display the initial card
+  displayCard(cardIndex);
 
   var timeEl = document.querySelector('.timer');
   var secondsLeft = 10;
@@ -148,6 +167,7 @@ document.getElementById('Home-Page-Button').addEventListener('click', function (
         showTimeIsUpPage();
       }
     }, 1000);
+    timerActive = true;
   }
 
   function showTimeIsUpPage() {
@@ -159,72 +179,19 @@ document.getElementById('Home-Page-Button').addEventListener('click', function (
   setTime();
 });
 
-  document.getElementById('Start-Over-Button').addEventListener('click', function() {
-    // Show the home page card
-    var homePageCard = document.getElementById('Home-Page-Card');
-    homePageCard.style.display = 'block';
-  
-    // Hide the time-out message container
-    var timeOutPage = document.getElementById('Time-Is-Up');
-    timeOutPage.style.display = 'none';
-  
-    // Reset the quiz by resetting the card index
-    cardIndex = 0;
-  
-    // Displays the initial card
-    displayCard(cardIndex);
+document.getElementById('Start-Over-Button').addEventListener('click', function() {
+  var homePageCard = document.getElementById('Home-Page-Card');
+  homePageCard.style.display = 'block';
 
-  
+  var timeOutPage = document.getElementById('Time-Is-Up');
+  timeOutPage.style.display = 'none';
 
-
+  cardIndex = 0;
+  displayCard(cardIndex);
 });
 
 
 
-//TODO 
-// *timer penalty for when they get a question wrong
-// * add to the timeout nested function so that it if the user completes before time is up, then it does not display the page...
-// *build an All done! page
-
-
-// // *build a high scores page
-// // * create a text box for entering scores and initials
-// <div class="input-group">
-//             <label for="initials-And-Score">Enter initials and Score:</label>
-//             <input type="text" name="initials-And-Score" id="initials-And-Score" placeholder="Adam 50" />
-//           </div>//put this in your html high score page
-// // *build a function that stores the scores and initials 
-// var resultScores
-// var userInitials   
-// function storeScores(){}
-// //   * that function should also push the information so it is displayed on the high score page.
-// //   *link the text in the home-page to the high scores page. 
-// *give the questions weight so a total score can be determined.
-// *add or subtract based on the user input 
-// * change the font of the correct/incorrect to reflect the modules. (probably in css)
-// * stop the timer if they finish the quiz in time.
-// * fix bug that after starting over, it takes you to homepage but you cannot take the quiz again.
 
 
 
-
-//write the javascript that takes the input from text box and sends it to local storage
-
-//1.) select the html query so it is available for manipulation
-// var userInitialsInput = document.querySelector("#initials-And-Score");
-
-// // 2.) create a button with a click event
-// submitScoresButton.addEventListener("click", function(event){
-//   event.preventDefault();
-
-//   //3.)  create user object from submission
-//   var userInput = {
-//     initials: userInitialsInput.value.trim()
-    
-//   };
-
-//   // set new submission to local storage 
-//   localStorage.setItem("userInput", JSON.stringify(user));
-  
-
-// });
